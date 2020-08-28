c33ce8370a0d1734ef28488e53a9c51452064179
hitomi kanaya <hitomickeykey@gmail.com>
2020-08-28 17:12:10 +0900

    最寄り駅(nearest_station)の管理機能作成

---
+++ app/views/nearest_stations/_nearest_station.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! nearest_station, :id, :railway_line, :station_name, :time_to_station, :created_at, :updated_at
+json.url nearest_station_url(nearest_station, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_072710) do
+ActiveRecord::Schema.define(version: 2020_08_28_074029) do
+
+  create_table "nearest_stations", force: :cascade do |t|
+    t.string "railway_line"
+    t.string "station_name"
+    t.integer "time_to_station"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "properties", force: :cascade do |t|
     t.string "name"
