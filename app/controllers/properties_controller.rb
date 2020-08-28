9040aad1e62351e93946dd4ff1543e697063de1a
hitomi kanaya <hitomickeykey@gmail.com>
2020-08-28 20:22:05 +0900

    バリデーションの設定

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -57,7 +57,6 @@ class PropertiesController < ApplicationController
     @property.destroy
     respond_to do |format|
       format.html { redirect_to properties_url, notice: 'Property was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,10 @@
 class Property < ApplicationRecord
+  has_many :nearest_station, dependent: :destroy
+
+  validates :name, presence: true
+  validates :rent, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
+  validates :notes, presence: true
+
 end
